87966d36458f476137529769548728ca30e86439
Christian Ledesma <softcodify@gmail.com>
2023-12-23 02:15:41 -0300

    Se conectó con gpt-back

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const start = require("./api")
+const axios = require("axios")
+require('dotenv').config()
 
-async function wpResponse(msjRes) {
+async function wpResponse() {
     try {
         const client = await start()
         client.on('message', message => {
             if (message.body.startsWith("gpt:")) {
-                client.sendMessage(message.from, msjRes)
+                axios.post(`${process.env.API_URL}/api/ask`, message.body.slice(4))
+                    .then(response => {
+                        client.sendMessage(message.from, response)
+                        console.log('Respuesta: ', response.data)
+                    })
+                    .catch(error => {
+                        client.sendMessage(message.from, error.response.data.error)
+                        console.error('Error: ', error.response.data.error)
+                    })
             }
         })
     } catch (error) {
@@ -13,4 +23,4 @@ async function wpResponse(msjRes) {
     }
 }
 
-wpResponse('TEST')
+wpResponse()
